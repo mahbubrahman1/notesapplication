dc3b35a42d7be7f41cc3614ebf14bc7b8e056065
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2023-01-12 12:37:02 +0600

    working on rest framework

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
 def get_routes(request):
     routes = [
         {
@@ -36,4 +37,4 @@ def get_routes(request):
         },
     ]
     
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
