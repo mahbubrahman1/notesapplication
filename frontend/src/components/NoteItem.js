dc453f32b3bf173d7ebdf6532262d83621b70d44
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2023-01-15 13:02:00 +0600

    worked on single note page

--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NoteItem = ({ note }) => {
     return (
-        <div>
-            <h3>{note.body}</h3>
-        </div>
+        <Link to={`/note/${note.id}`}>
+            <div>
+                <h3>{note.body}</h3>
+            </div>
+        </Link>
     )
 }
 
--- frontend/src/pages/SingleNote.js
+++ frontend/src/pages/SingleNote.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const SingleNote = () => {
+    const { id } = useParams();
+    const [note, setNote] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:8000/api/note/${id}`)
+            .then(response => response.json())
+            .then(data => setNote(data))
+    }, [id])
+
     return (
         <div>
-            <h2>this is single note</h2>
+            <h2>{note?.body}</h2>
         </div>
     )
 }
