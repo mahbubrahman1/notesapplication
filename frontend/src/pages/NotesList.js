64ec7e6c3539771f41a5bee874c80f01030c0462
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2023-03-05 16:31:50 +0600

    added search

--- frontend/src/pages/NotesList.js
+++ frontend/src/pages/NotesList.js
@@ -1,25 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import CreateButton from '../components/CreateButton'
 import NoteItem from '../components/NoteItem'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNoteSticky } from '@fortawesome/free-solid-svg-icons'
+
 
 const NotesList = () => {
     const [notes, setNotes] = useState([])
+    const [matchedNotes, setMatchedNotes] = useState([])
+    const [searchText, setSearchText] = useState('')
+
 
     useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/notes/')
-            .then(response => response.json())
-            .then(data => setNotes(data))
+        getNotes()
     }, [])
+
+
+    const getNotes = async () => {
+        const response = await fetch(`http://127.0.0.1:8000/api/notes`)
+        const data = await response.json()
+        setNotes(data)
+        setMatchedNotes(data)
+    }
+
+
+    const handleSearchField = e => {
+        const searchInput = e.target.value
+        setSearchText(searchInput)
+        const matched_notes = notes.filter(note => note.body.toLowerCase().includes(searchInput.toLowerCase()))
+        setMatchedNotes(matched_notes)
+    }
+
 
     return (
         <div>
             <div className="notes-header">
-                <h2 className='notes-title'>&#9782; Notes</h2>
+                <h2 className='notes-title'><FontAwesomeIcon icon={faNoteSticky} /> Notes</h2>
                 <p className='notes-count'>{notes.length}</p>
+            </div>
+            <div>
+                <input onChange={handleSearchField} type='text'></input>
             </div>
             <div className='notes-list'>
                 {
-                    notes.map(note => (
+                    matchedNotes.map(note => (
                         <NoteItem key={note.id} note={note} />
                     ))
                 }
@@ -28,5 +52,6 @@ const NotesList = () => {
         </div>
     )
 }
+
 
 export default NotesList
